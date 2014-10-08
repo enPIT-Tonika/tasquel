d77c7ea6ac1ea323fe51bfbf02b09052b6639de0
lapdog717 <a0956yw@aiit.ac.jp>
2014-10-08 12:21:41 +0000

    Update README

--- db/schema.rb
+++ db/schema.rb
@@ -23,60 +23,10 @@ ActiveRecord::Schema.define(version: 20141005142809) do
     t.datetime "updated_at"
   end
 
-  create_table "daily_infos", force: true do |t|
-    t.integer  "exercise_id"
-    t.integer  "consumed_minutes"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "exercise_infos", force: true do |t|
-    t.string   "exercise"
-    t.integer  "calorie"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "exercises", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "facilities", force: true do |t|
-    t.string   "name"
-    t.string   "address"
-    t.string   "tel_no"
-    t.integer  "ostomate_toilet"
-    t.integer  "ccheer_toilet"
-    t.integer  "cbet_toilet"
-    t.integer  "wchair_lift"
-    t.integer  "wchair_escalator"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "zip_code"
-    t.text     "description"
-  end
-
-  create_table "profiles", force: true do |t|
-    t.string   "sex"
-    t.integer  "age"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "task_boards", force: true do |t|
     t.text     "taskText"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "used_calorie_infos", force: true do |t|
-    t.integer  "used_calorie"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "profile_id"
-    t.integer  "exercise_id"
   end
 
 end
