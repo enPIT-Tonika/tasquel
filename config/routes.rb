612637f982bc5e567169f7529a2f13f82446dc1d
lapdog717 <a0956yw@aiit.ac.jp>
2014-11-09 13:36:05 +0000

    時間を入力しないタスクのソート状態を保存

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
 
   resources :families
 
-  resources :task_boards
+  #resources :task_boards
+  resources :task_boards do
+    put :sort
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
