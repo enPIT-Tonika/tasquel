9bd68da4f81f1d809f98ca0a5222248c29d8bba8
Yoshiki Eguchi <yoshiki.eguchi@gmail.com>
2014-11-24 15:46:56 +0000

    時刻情報を入力・削除できる機能を追加
    * home/indexのページに時刻情報を追加・編集するフォームを実装
    * home_controllerに時刻情報の追加・編集ロジックを追加
    [TODO]
    * HerokuがUTCで時間が動作しているため、日本との時差を加味する必要がある
     * 現状、workaroundとして9:00を強制的に引いて時間を計算するようにした

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < BaseController
   def index
     login_required
     
+    #通知ステータスの情報
     unless @current_user.blank?
       if @current_user.notify == true
           @notify_status = "ON"
@@ -10,6 +11,11 @@ class HomeController < BaseController
           @notify_status = "OFF"
           @link_msg = "ONにする"
        end
+    end
+    #時刻情報の取得
+    @time_info = []
+    unless @current_user.json_time.blank?
+      @time_info = @current_user.json_time
     end
   end
   
@@ -26,5 +32,36 @@ class HomeController < BaseController
      @current_user.update_attribute(:notify,  true)
     end
     redirect_to home_index_path
+  end
+  
+  def del_notify
+    #指定された通知エントリを削除
+    login_required
+    
+    idx = params[:idx].to_i
+    j = @current_user.json_time
+    j.delete_at(idx)
+    logger.info("current json_data : #{j}")
+     j = JSON.dump(j) if j.length > 1
+    @current_user.update_attribute(:json_time, [])
+    @current_user.update_attribute(:json_time, j) if j.length > 0
+    
+    redirect_to home_index_path
+  end
+  
+  def add_notify
+    #通知エントリを追加
+    login_required
+    j = []
+    unless @current_user.json_time.blank?
+      j = @current_user.json_time
+    end
+    t = params[:time]
+    j.push({desc: params[:desc], time: params[:time]})
+    j = JSON.dump(j) if j.length > 1
+    #j.sort! {|a,b| Time.parse(a["time"]) <=> Time.parse(b["time"])}
+   @current_user.update_attribute(:json_time, j)
+   
+   redirect_to home_index_path   
   end
 end
