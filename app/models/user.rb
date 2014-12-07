26b9996b988d8700b4c4b39f25bfd907ab70d02e
Yoshiki Eguchi <yoshiki.eguchi@gmail.com>
2014-12-07 15:32:08 +0000

    初登録時、使い方をつぶやく機能を実装（非アクティブ）
    * 現在書き込み規制中のため、非アクティブ状態でとりあえずアップロード

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,19 @@ class User < ActiveRecord::Base
   has_many :done_list
   
   def self.create_with_omniauth(auth)
+    instruction(auth['info']['nickname'])
     create! do |user|
       user.provider = auth['provider']
       user.uid = auth['uid']
       user.screen_name = auth['info']['nickname']
       user.name = auth['info']['name']
     end
+  end
+  
+  private
+  def self.instruction(screen_name)
+    msg = "#{screen_name}さん、登録ありがとう！登録した時間に「お薬のんだ？」とつぶやきます。のんだら返信してね！"
+    p msg
+    #$tw_client.update(msg)
   end
 end
